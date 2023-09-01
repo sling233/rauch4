sling233/rauch4

commit a210c2d6d2cd201201c0f54cfbec798a8d27c490

Stats are better for non-participating players
Also removed some left over debug code

data/rauch/functions/game/framework/stats/tie.mcfunction
@@ -8,7 +8,7 @@ execute as @a run scoreboard players operation @s stats_g_damage_t /= Global 20
 execute as @a run tellraw @s ""
 execute as @a run tellraw @s [{"text":"---------------- ","color":"yellow"},{"text":"Tie","color":"yellow"},{"text":" -----------------","color":"yellow"}]
 execute if score Global mode matches 0 run tellraw @a [{"text":"Survivors: ","color":"yellow"},{"selector":"@a[tag=game,tag=!dead]"}]
-execute if score Global mode matches 1..2 run tellraw @a [{"text":"Winner: ","color":"yellow"},{"text":"Undetermined"}]
+execute as @a run tellraw @s [{"text":"Winner: ","color":"yellow"},{"text":"Team Blue","color":"blue"}]
 execute if score Global mode matches 1 as @a run tellraw @s [{"text":"Points Scored: ","color":"yellow"},{"score":{"name":"@s","objective":"stats_g_points_scored"}}]
 execute if score Global mode matches 4 as @a run tellraw @s [{"text":"Points Scored: ","color":"yellow"},{"score":{"name":"@s","objective":"stats_g_points_scored"}}]
 execute as @a run tellraw @s [{"text":"Kills: ","color":"yellow"},{"score":{"name":"@s","objective":"stats_g_kills"}}]

commit 7d6d4fdcc12f8a614800d2fd350bc24d36682bd5

Capture the flag update prototype, a few fixes
The game is over after 3 minutes or 5 points (customizable) for one team. There is a tiebreaker, the closest distance to the enemy base.
All information is showed via a bossbar instead of the sidebar.
If you walk too far into your own base with the flag, it will reset to the center.
Dying with the flag is still buggy right now.
The flag now always faces the red base (instead of one fixed direction)
Fixed the wrong team being displayed as winner when blue wins or there is tie (introduced in https://github.com/sling233/rauch4/commit/a210c2d6d2cd201201c0f54cfbec798a8d27c490)

data/rauch/functions/game/framework/end_game.mcfunction
@@ -23,8 +23,10 @@ scoreboard players reset Global killNumRed
 scoreboard players reset Global killNumBlue
 scoreboard players reset Global killsDisplay
 scoreboard players reset Global gameend
+
 scoreboard objectives setdisplay sidebar
 bossbar set minecraft:center_control visible false
+bossbar set minecraft:flag_location visible false
 function rauch:game/ui/bossbar/allinvisible
 execute as @a[tag=game] run function rauch:game/framework/move_player
 execute as @a[tag=spectator] run function rauch:game/framework/spectate_stop

data/rauch/functions/game/framework/gameinit/countdown.mcfunction
@@ -29,6 +29,7 @@ execute if score Global gamestart matches 0 run scoreboard players reset Global 
 
 execute if score Global mode matches 1 run scoreboard players set Global time_red 400200
 execute if score Global mode matches 1 run scoreboard players set Global time_blue 400200
+execute if score Global mode matches 4 run scoreboard players set Global ctf_time 3600
 scoreboard players set @a[tag=game] cool1 2
 scoreboard players set @a[tag=game] cool2 2
 scoreboard players set @a[tag=game] cool3 2

data/rauch/functions/game/framework/start_game.mcfunction
@@ -42,7 +42,7 @@ execute if score Global mode matches 2 run function rauch:game/mode/respawn_deat
 execute if score Global mode matches 3 run function rauch:game/mode/practice/conditions
 execute if score Global mode matches 4 run function rauch:game/mode/capture_the_flag/conditions
 
-# elytra bossbar after setup so ma is properly set
+# elytra bossbar after setup so max is properly set
 function rauch:game/ui/bossbar/elytra/set_max
 
 scoreboard players set Global game_running 1

data/rauch/functions/game/framework/stats/tie.mcfunction
@@ -8,7 +8,7 @@ execute as @a run scoreboard players operation @s stats_g_damage_t /= Global 20
 execute as @a run tellraw @s ""
 execute as @a run tellraw @s [{"text":"---------------- ","color":"yellow"},{"text":"Tie","color":"yellow"},{"text":" -----------------","color":"yellow"}]
 execute if score Global mode matches 0 run tellraw @a [{"text":"Survivors: ","color":"yellow"},{"selector":"@a[tag=game,tag=!dead]"}]
-execute as @a run tellraw @s [{"text":"Winner: ","color":"yellow"},{"text":"Team Blue","color":"blue"}]
+execute as @a run tellraw @s [{"text":"Winner: ","color":"yellow"},{"text":"Tie","color":"yellow"}]
 execute if score Global mode matches 1 as @a run tellraw @s [{"text":"Points Scored: ","color":"yellow"},{"score":{"name":"@s","objective":"stats_g_points_scored"}}]
 execute if score Global mode matches 4 as @a run tellraw @s [{"text":"Points Scored: ","color":"yellow"},{"score":{"name":"@s","objective":"stats_g_points_scored"}}]
 execute as @a run tellraw @s [{"text":"Kills: ","color":"yellow"},{"score":{"name":"@s","objective":"stats_g_kills"}}]

data/rauch/functions/game/framework/stats/win_red.mcfunction
@@ -14,7 +14,7 @@ execute as @a[team=Red] run tellraw @s [{"text":"---------------- ","color":"yel
 execute as @a[team=Blue] run tellraw @s [{"text":"---------------- ","color":"yellow"},{"text":"Defeat","color":"red"},{"text":" -----------------","color":"yellow"}]
 execute as @a[tag=!game] run tellraw @s [{"text":"--------------- ","color":"yellow"},{"text":"Game Ended","color":"yellow"},{"text":" --------------","color":"yellow"}]
 execute if score Global mode matches 0 run tellraw @a [{"text":"Survivors: ","color":"yellow"},{"selector":"@a[tag=game,tag=!dead]"}]
-execute as @a run tellraw @s [{"text":"Winner: ","color":"yellow"},{"text":"Team Blue","color":"blue"}]
+execute as @a run tellraw @s [{"text":"Winner: ","color":"yellow"},{"text":"Team Red","color":"red"}]
 execute as @a[tag=game] run tellraw @s [{"text":"Kills: ","color":"yellow"},{"score":{"name":"@s","objective":"stats_g_kills"}}]
 execute as @a[tag=game] run tellraw @s [{"text":"Deaths: ","color":"yellow"},{"score":{"name":"@s","objective":"stats_g_deaths"}}]
 execute if score Global mode matches 1 as @a[tag=game] run tellraw @s [{"text":"Points Scored: ","color":"yellow"},{"score":{"name":"@s","objective":"stats_g_points_scored"}}]

data/rauch/functions/game/mode/capture_the_flag/capture_the_flag.mcfunction
@@ -2,12 +2,38 @@ execute as @a[tag=flagPickedUp] run function rauch:game/mode/capture_the_flag/fl
 execute at @e[type=minecraft:armor_stand,tag=flag,limit=1] positioned ~-1 ~ ~-1 as @a[tag=game,tag=!dead,dx=1,dy=2,dz=1,limit=1] run function rauch:game/mode/capture_the_flag/flag_pickup
 
 function rauch:game/mechanics/respawn/respawn_main
-scoreboard players operation Red: flagDisplay = Global flag_points_red
-scoreboard players operation Blue: flagDisplay = Global flag_points_blue
 
-execute if score Global flagDistanceRed matches ..9 as @a[tag=flagPickedUp] run function rauch:game/mode/capture_the_flag/red_score
-execute if score Global flagDistanceBlue matches ..9 as @a[tag=flagPickedUp] run function rauch:game/mode/capture_the_flag/blue_score
+# flag at enemy base
+execute if score Global flagDistanceRed matches ..900 as @a[tag=flagPickedUp,team=Blue] run function rauch:game/mode/capture_the_flag/blue_score
+execute if score Global flagDistanceBlue matches ..900 as @a[tag=flagPickedUp,team=Red] run function rauch:game/mode/capture_the_flag/red_score
 
-execute unless score Global gameend matches 1.. if score Global flag_points_red matches 5.. if score Global flag_points_blue matches 5.. run function rauch:game/framework/stats/tie
-execute unless score Global gameend matches 1.. if score Global flag_points_red matches 5.. unless score Global flag_points_blue matches 5.. run function rauch:game/framework/stats/win_red
-execute unless score Global gameend matches 1.. if score Global flag_points_blue matches 5.. unless score Global flag_points_red matches 5.. run function rauch:game/framework/stats/win_blue
+# flag too far in own base (drop flag)
+scoreboard objectives add dis_diff dummy
+scoreboard players operation Global dis_diff = Global flagDistanceBlue
+scoreboard players operation Global dis_diff -= Global flagDistanceRed
+
+execute if entity @a[tag=flagPickedUp,team=Red] if score Global dis_diff matches 14000.. run function rauch:game/mode/capture_the_flag/flag_reset
+execute if entity @a[tag=flagPickedUp,team=Blue] if score Global dis_diff matches ..-14000 run function rauch:game/mode/capture_the_flag/flag_reset
+
+# draw lines and handle bossbar
+execute as @e[type=minecraft:marker,tag=ce] run function rauch:game/mode/capture_the_flag/visual/draw_max_lines
+function rauch:game/mode/capture_the_flag/visual/bossbar
+function rauch:game/mode/capture_the_flag/visual/update_bossbar_title
+#^ i don't have to run this every tick, but it's not that expensive ^
+
+scoreboard objectives remove dis_diff
+
+execute if score Global ctf_time matches 1.. run scoreboard players remove Global ctf_time 1
+
+########## game end ##########
+# time
+execute if score Global gameend matches 1.. run return 1
+execute if score Global ctf_time matches ..0 if score Global flag_points_red > Global flag_points_blue run function rauch:game/framework/stats/win_red
+execute if score Global ctf_time matches ..0 if score Global flag_points_red < Global flag_points_blue run function rauch:game/framework/stats/win_blue
+execute if score Global ctf_time matches ..0 if score Global flag_points_red = Global flag_points_blue if score Global flag_min_distance_red > Global flag_min_distance_blue run function rauch:game/framework/stats/win_red
+execute if score Global ctf_time matches ..0 if score Global flag_points_red = Global flag_points_blue if score Global flag_min_distance_red < Global flag_min_distance_blue run function rauch:game/framework/stats/win_blue
+execute if score Global ctf_time matches ..0 if score Global flag_points_red = Global flag_points_blue if score Global flag_min_distance_red = Global flag_min_distance_blue run function rauch:game/framework/stats/tie
+
+# knockout
+execute if score Global flag_points_red >= Global ctf_points_to_win run function rauch:game/framework/stats/tie
+execute if score Global flag_points_red >= Global ctf_points_to_win run function rauch:game/framework/stats/win_red

data/rauch/functions/game/mode/capture_the_flag/flag_drop.mcfunction
@@ -1,18 +1,8 @@
-function rauch:game/mode/capture_the_flag/spawn_flag
-
-tag @s remove flagPickedUp
-
-effect clear @s minecraft:glowing
-
+execute at @s run function rauch:game/mode/capture_the_flag/spawn_flag
+function rauch:game/mode/capture_the_flag/remove_flag
 title @a[tag=game] title ""
 title @a[tag=game] subtitle "Flag Dropped"
 title @a[tag=spectator] title ""
 title @a[tag=spectator] subtitle "Flag Dropped"
-
 scoreboard players set Global flagDistanceRed 1000000
 scoreboard players set Global flagDistanceBlue 1000000
-
-scoreboard players reset @s hack
-function rauch:game/mechanics/hackend
-
-function rauch:game/mechanics/armor

data/rauch/functions/game/mode/capture_the_flag/flag_picked_up.mcfunction
@@ -1,19 +1,42 @@
-execute store result score Global xPos run data get entity @s Pos[0]
-execute as @s[team=Red] store result score Global yPos run data get entity @e[type=marker,tag=map,tag=t2,limit=1] Pos[0]
-execute as @s[team=Blue] store result score Global yPos run data get entity @e[type=marker,tag=map,tag=t1,limit=1] Pos[0]
-scoreboard players operation Global xPos -= Global yPos
+# calculate distance
+scoreboard objectives add xPos2 dummy
+scoreboard objectives add zPos2 dummy
+# x
+execute store result score @s xPos run data get entity @s Pos[0] 1000
+scoreboard players operation @s xPos2 = @s xPos
+execute store result score Global xPos run data get entity @e[type=marker,tag=map,tag=t1,limit=1] Pos[0] 1000
+execute store result score Global xPos2 run data get entity @e[type=marker,tag=map,tag=t2,limit=1] Pos[0] 1000
+scoreboard players operation @s xPos -= Global xPos
+scoreboard players operation @s xPos2 -= Global xPos2
+# z
+execute store result score @s zPos run data get entity @s Pos[2] 1000
+scoreboard players operation @s zPos2 = @s zPos
+execute store result score Global zPos run data get entity @e[type=marker,tag=map,tag=t1,limit=1] Pos[2] 1000
+execute store result score Global zPos2 run data get entity @e[type=marker,tag=map,tag=t2,limit=1] Pos[2] 1000
+scoreboard players operation @s zPos -= Global zPos
+scoreboard players operation @s zPos2 -= Global zPos2
 
-execute store result score Global zPos run data get entity @s Pos[2]
-execute as @s[team=Red] store result score Global yPos run data get entity @e[type=marker,tag=map,tag=t2,limit=1] Pos[2]
-execute as @s[team=Blue] store result score Global yPos run data get entity @e[type=marker,tag=map,tag=t1,limit=1] Pos[2]
-scoreboard players operation Global zPos -= Global yPos
+execute at @s run summon item_display ~ ~ ~ {UUID:[I;0,0,0,0]}
 
-scoreboard players operation Global xPos *= Global xPos
-scoreboard players operation Global zPos *= Global zPos
+# calculate distance 1
+execute store result storage temp distance.x float 0.001 run scoreboard players get @s xPos
+execute store result storage temp distance.z float 0.001 run scoreboard players get @s zPos
+function rauch:game/mode/capture_the_flag/get_distance_macro with storage temp distance
+execute store result score Global flagDistanceRed run data get entity 0-0-0-0-0 transformation.scale[0] 1000
 
-scoreboard players operation Global xPos += Global zPos
+# calculate distance 2
+execute store result storage temp distance.x float 0.001 run scoreboard players get @s xPos2
+execute store result storage temp distance.z float 0.001 run scoreboard players get @s zPos2
+function rauch:game/mode/capture_the_flag/get_distance_macro with storage temp distance
+execute store result score Global flagDistanceBlue run data get entity 0-0-0-0-0 transformation.scale[0] 1000
 
-execute as @s[team=Red] run scoreboard players operation Global flagDistanceRed = Global xPos
-execute as @s[team=Blue] run scoreboard players operation Global flagDistanceBlue = Global xPos
+kill 0-0-0-0-0
+scoreboard objectives remove xPos2
+scoreboard objectives remove zPos2
 
+# save min distance
+execute as @s[team=Blue] run scoreboard players operation Global flag_min_distance_red < Global flagDistanceRed
+execute as @s[team=Red] run scoreboard players operation Global flag_min_distance_blue < Global flagDistanceBlue
+
+# hack player
 scoreboard players set @s hack 200

data/rauch/functions/game/mode/capture_the_flag/flag_pickup.mcfunction
@@ -1,7 +1,9 @@
 kill @e[type=armor_stand,tag=flag]
 tag @s add flagPickedUp
 effect give @s minecraft:glowing infinite 0 true
-#effect give @s minecraft:jump_boost 1000000 128 true
+
+#scoreboard players set Global flag_min_distance_red 9999999
+#scoreboard players set Global flag_min_distance_blue 9999999
 
 title @a[tag=game] title ""
 title @a[tag=spectator] title ""

data/rauch/functions/game/mode/capture_the_flag/flag_reset.mcfunction
@@ -1,6 +1,11 @@
+execute at @e[type=marker,tag=map,tag=ce] positioned ~ ~-20 ~ run function rauch:game/mode/capture_the_flag/spawn_flag
+execute as @a[tag=flagPickedUp] run function rauch:game/mode/capture_the_flag/remove_flag
+title @a[tag=game] title ""
+title @a[tag=game] subtitle "Flag Reset to Center"
+title @a[tag=spectator] title ""
+title @a[tag=spectator] subtitle "Flag Reset to Center"
 scoreboard players set Global flagDistanceRed 1000000
 scoreboard players set Global flagDistanceBlue 1000000
-
 scoreboard objectives add 50 dummy
 scoreboard players set Global 50 50
 scoreboard players operation Global flag_min_distance_red = Global spawn_to_center_distance
@@ -8,17 +13,3 @@ scoreboard players operation Global flag_min_distance_red *= Global 50
 scoreboard players operation Global flag_min_distance_blue = Global spawn_to_center_distance
 scoreboard players operation Global flag_min_distance_blue *= Global 50
 scoreboard objectives remove 50
-
-scoreboard players set Global flag_points_red 0
-scoreboard players set Global flag_points_blue 0
-
-scoreboard players set Global ctf_time 3600
-scoreboard players set Global gamestart 120
-
-scoreboard players set @a stats_g_points_scored 0
-
-function rauch:game/mode/capture_the_flag/visual/bossbar_setup
-
-# some problems when map is unloaded
-# function rauch:game/mode/capture_the_flag/spawn_flag
-execute summon armor_stand run function rauch:game/mode/capture_the_flag/spawn_flag_unloaded

data/rauch/functions/game/mode/capture_the_flag/get_distance_macro.mcfunction
@@ -1,0 +1,1 @@
+$data modify entity 0-0-0-0-0 transformation set value [$(x),0f,0f,0f,0f,0f,0f,0f,$(z),0f,0f,0f,0f,0f,0f,1f]

data/rauch/functions/game/mode/capture_the_flag/remove_flag.mcfunction
@@ -1,0 +1,5 @@
+tag @s remove flagPickedUp
+effect clear @s minecraft:glowing
+scoreboard players reset @s hack
+function rauch:game/mechanics/hackend
+function rauch:game/mechanics/armor

data/rauch/functions/game/mode/capture_the_flag/score.mcfunction
@@ -1,4 +1,5 @@
-function rauch:game/mode/capture_the_flag/flag_drop
+function rauch:game/mode/capture_the_flag/flag_reset
 execute as @a[tag=game,predicate=rauch:stun] run function rauch:game/mechanics/unstun
 execute as @a[tag=game,tag=zarzahn_hook] run function rauch:game/ability/zarzahn/r/cancel_hook
 scoreboard players add @s stats_g_points_scored 1
+function rauch:game/mode/capture_the_flag/visual/update_bossbar_title

data/rauch/functions/game/mode/capture_the_flag/spawn_flag.mcfunction
@@ -1,6 +1,7 @@
+#summons flag at the position the function is executed at
 kill @e[type=armor_stand,tag=flag]
 execute unless entity @e[type=marker,tag=map,tag=ce] run say i dont exist
-execute at @e[type=marker,tag=map,tag=ce] run summon armor_stand ~ ~-20 ~ {Silent:1b,Marker:1b,Invisible:1b,Tags:["flag","setup"],ArmorItems:[{},{},{},{id:"minecraft:white_banner",Count:1b}]}
-execute as @e[type=armor_stand,tag=flag,tag=setup] at @s run tp @s ~ ~ ~ 90 0
+summon armor_stand ~ ~ ~ {Silent:1b,Marker:1b,Invisible:1b,Tags:["flag","setup"],ArmorItems:[{},{},{},{id:"minecraft:white_banner",Count:1b}]}
+execute as @e[type=armor_stand,tag=flag,tag=setup] at @s facing entity @e[type=marker,tag=map,tag=t1] feet run tp @s ~ ~ ~ ~ 0
 execute as @e[type=armor_stand,tag=flag,tag=setup] run effect give @e[type=minecraft:armor_stand,tag=flag] minecraft:glowing 1000000 0
 tag @e[type=armor_stand,tag=flag,tag=setup] remove setup

data/rauch/functions/game/mode/capture_the_flag/spawn_flag_unloaded.mcfunction
@@ -3,5 +3,5 @@ data merge entity @s {Silent:1b,Marker:1b,Invisible:1b,ArmorItems:[{},{},{},{id:
 tag @s add flag
 data modify entity @s Pos set from storage map_data active.center
 execute at @s run tp @s ~ ~-20 ~
-execute at @s run tp @s ~ ~ ~ 90 0
+execute at @s facing entity @e[type=marker,tag=map,tag=t1] feet run tp @s ~ ~ ~ ~ 0
 effect give @s minecraft:glowing 1000000 0

data/rauch/functions/game/mode/capture_the_flag/visual/bossbar.mcfunction
@@ -1,0 +1,15 @@
+#tellraw @a [{"score":{"name":"Global","objective":"dis_diff"}}]
+scoreboard objectives add t_calc dummy
+scoreboard objectives add 100 dummy
+scoreboard players set Global 100 100
+scoreboard players operation Global t_calc = Global dis_diff
+scoreboard players operation Global t_calc /= Global 100
+#tellraw @a [{"score":{"name":"Global","objective":"t_calc"}}]
+
+scoreboard players operation Global t_calc += Global spawn_to_center_distance
+
+execute store result bossbar flag_location value run scoreboard players get Global t_calc
+#tellraw @a [{"score":{"name":"Global","objective":"t_calc"}}]
+
+scoreboard objectives remove 100
+scoreboard objectives remove t_calc

data/rauch/functions/game/mode/capture_the_flag/visual/bossbar_setup.mcfunction
@@ -1,0 +1,33 @@
+bossbar set flag_location players @a[tag=game]
+bossbar set flag_location visible true
+
+scoreboard objectives add t_bossbar_max dummy
+# calculate distance ####################
+# get coords
+scoreboard objectives add xPos2 dummy
+scoreboard objectives add zPos2 dummy
+# x
+execute store result score Global xPos run data get storage map_data active.red[0] 1000
+execute store result score Global xPos2 run data get storage map_data active.blue[0] 1000
+scoreboard players operation Global xPos -= Global xPos2
+# z
+execute store result score Global zPos run data get storage map_data active.red[2] 1000
+execute store result score Global zPos2 run data get storage map_data active.blue[2] 1000
+scoreboard players operation Global zPos -= Global zPos2
+
+execute at @p run summon item_display ~ ~ ~ {UUID:[I;0,0,0,0]}
+
+# calculate distance
+execute store result storage temp distance.x float 0.001 run scoreboard players get Global xPos
+execute store result storage temp distance.z float 0.001 run scoreboard players get Global zPos
+function rauch:game/mode/capture_the_flag/get_distance_macro with storage temp distance
+execute store result score Global t_bossbar_max run data get entity 0-0-0-0-0 transformation.scale[0] 20
+
+kill 0-0-0-0-0
+scoreboard objectives remove xPos2
+scoreboard objectives remove zPos2
+###################################
+execute store result bossbar flag_location max run scoreboard players get Global t_bossbar_max
+scoreboard players operation Global spawn_to_center_distance = Global t_bossbar_max
+scoreboard players operation Global spawn_to_center_distance /= Global 2
+scoreboard objectives remove t_bossbar_max

data/rauch/functions/game/mode/capture_the_flag/visual/draw_max_lines.mcfunction
@@ -1,0 +1,6 @@
+scoreboard objectives add t_steps dummy
+execute at @s facing entity @e[type=marker,tag=t1] feet rotated ~ 0 positioned ^15 ^-19.9 ^6.5 run function rauch:game/mode/capture_the_flag/visual/draw_orthogonal_red
+scoreboard players reset Global t_steps
+execute at @s facing entity @e[type=marker,tag=t2] feet rotated ~ 0 positioned ^15 ^-19.9 ^6.5 run function rauch:game/mode/capture_the_flag/visual/draw_orthogonal_blue
+
+scoreboard objectives remove t_steps

data/rauch/functions/game/mode/capture_the_flag/visual/draw_orthogonal_blue.mcfunction
@@ -1,0 +1,3 @@
+particle minecraft:dust 0.2 0.2 1 0.4 ~ ~ ~ 0.1 0.1 0.1 0 1 force
+scoreboard players add Global t_steps 1
+execute if score Global t_steps matches ..60 positioned ^-0.5 ^ ^ run function rauch:game/mode/capture_the_flag/visual/draw_orthogonal_blue

data/rauch/functions/game/mode/capture_the_flag/visual/draw_orthogonal_red.mcfunction
@@ -1,0 +1,3 @@
+particle minecraft:dust 1 0.2 0.2 0.4 ~ ~ ~ 0.1 0.1 0.1 0 1 force
+scoreboard players add Global t_steps 1
+execute if score Global t_steps matches ..60 positioned ^-0.5 ^ ^ run function rauch:game/mode/capture_the_flag/visual/draw_orthogonal_red

data/rauch/functions/game/mode/capture_the_flag/visual/update_bossbar_title.mcfunction
@@ -1,0 +1,34 @@
+scoreboard objectives add t_red_points_display dummy
+scoreboard objectives add t_blue_points_display dummy
+scoreboard objectives add 1000 dummy
+scoreboard players set Global 1000 1000
+
+scoreboard players operation Global t_red_points_display = Global flag_min_distance_blue
+scoreboard players operation Global t_red_points_display /= Global 1000
+scoreboard players operation Global t_blue_points_display = Global flag_min_distance_red
+scoreboard players operation Global t_blue_points_display /= Global 1000
+
+
+scoreboard objectives add t_min dummy
+scoreboard objectives add t_sec dummy
+scoreboard objectives add extra_0 dummy
+scoreboard objectives add 1200 dummy
+scoreboard players set Global 1200 1200
+
+scoreboard players operation Global t_min = Global ctf_time
+scoreboard players operation Global t_sec = Global ctf_time
+scoreboard players operation Global t_min /= Global 1200
+scoreboard players operation Global t_sec %= Global 1200
+scoreboard players operation Global t_sec /= Global 20
+execute if score Global t_sec matches ..9 run scoreboard players set Global extra_0 0
+
+bossbar set flag_location name [{"score":{"name":"Global","objective":"flag_points_blue"},"color":"blue"},{"text":"   "},{"score":{"name":"Global","objective":"t_blue_points_display"},"color":"aqua"},{"text":"            ","color":"yellow"},{"score":{"name":"Global","objective":"t_min"},"color":"yellow"},{"text":":","color":"yellow"},{"score":{"name":"Global","objective":"extra_0"},"color":"yellow"},{"score":{"name":"Global","objective":"t_sec"},"color":"yellow"},{"text":"            "},{"score":{"name":"Global","objective":"t_red_points_display"},"color":"#FFAA00"},{"text":"   "},{"score":{"name":"Global","objective":"flag_points_red"},"color":"red"}]
+
+
+scoreboard objectives remove t_red_points_display
+scoreboard objectives remove t_blue_points_display
+scoreboard objectives remove 1000
+scoreboard objectives remove t_min
+scoreboard objectives remove t_sec
+scoreboard objectives remove extra_0
+scoreboard objectives remove 1200

data/rauch/functions/settings/removeobj.mcfunction
@@ -103,11 +103,15 @@ scoreboard objectives remove killNumBlue
 scoreboard objectives remove killsDisplay
 scoreboard objectives remove rd_kills_to_win
 #capturethefalg
+scoreboard objectives remove ctf_points_to_win
+scoreboard objectives remove ctf_time
 scoreboard objectives remove flagDistanceBlue
 scoreboard objectives remove flagDistanceRed
-scoreboard objectives remove flag_points_red
+scoreboard objectives remove flag_min_distance_blue
+scoreboard objectives remove flag_min_distance_red
 scoreboard objectives remove flag_points_blue
-scoreboard objectives remove flagDisplay
+scoreboard objectives remove flag_points_red
+scoreboard objectives remove spawn_to_center_distance
 
 scoreboard objectives setdisplay below_name
 scoreboard objectives setdisplay list

data/rauch/functions/settings/versions/v600.mcfunction
@@ -48,6 +48,7 @@ scoreboard objectives remove 2048
 scoreboard objectives remove bolt_hitby
 scoreboard objectives remove bolt_hit
 scoreboard objectives remove elytracool
+scoreboard objectives remove flagDisplay
 
 # changed types
 scoreboard objectives remove mapParticle

data/rauch/functions/setup.mcfunction
@@ -104,11 +104,19 @@ scoreboard objectives add killsDisplay dummy {"text":"Kills","color":"yellow"}
 scoreboard objectives add rd_kills_to_win dummy
 execute unless score Global rd_kills_to_win matches 1.. run scoreboard players set Global rd_kills_to_win 20
 #capture the flag
+scoreboard objectives add ctf_points_to_win dummy
+scoreboard objectives add ctf_time dummy
 scoreboard objectives add flagDistanceBlue dummy
 scoreboard objectives add flagDistanceRed dummy
-scoreboard objectives add flag_points_red dummy
+# scoreboard objectives add flag_max_distance_to_own_base dummy
+scoreboard objectives add flag_min_distance_red dummy
+scoreboard objectives add flag_min_distance_blue dummy
 scoreboard objectives add flag_points_blue dummy
-scoreboard objectives add flagDisplay dummy {"text":"Points","color":"yellow"}
+scoreboard objectives add flag_points_red dummy
+# scoreboard objectives add flag_reset_time dummy
+# scoreboard objectives add flag_spawn_time dummy
+scoreboard objectives add spawn_to_center_distance dummy
+execute unless score Global ctf_points_to_win matches 1.. run scoreboard players set Global ctf_points_to_win 5
 
 # setup stuff
 execute unless entity @a[tag=admin] unless entity @e[type=armor_stand,tag=main] run give @p minecraft:squid_spawn_egg{display:{Name:'{"text":"Lobby Spawn","color":"blue","italic":false}'},EntityTag:{Tags:["main"]}} 1
@@ -192,6 +200,11 @@ gamerule randomTickSpeed 0
 
 
 #bossbars
+bossbar add flag_location ""
+bossbar set flag_location name [{"text":"Flag Test","color":"yellow"}]
+bossbar set flag_location color white
+bossbar set flag_location style notched_10
+
 bossbar add center_control ""
 bossbar set center_control name [{"text":"Neutral","color":"yellow"}]
 bossbar set minecraft:center_control color white