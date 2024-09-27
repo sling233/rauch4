sling233/rauch4

commit f0a6ce25b424453da608eacd86e620aeee09617b

Standardized const and temp scores (v606)
Numbers are always <number> const. Every time a number is used in an equasion it's score gets defined somewhere at the beginning of the function.
Temporary scores are always t_<something> temp. Temporary scores should always reset near the end of the function. I might change that to being cleared at the end of each tick.

THERE MIGHT BE REALLY WEIRD BUGS! I probably missed some things.

v606: removed objectives blu_calc, red_calc, xPos, yPos, zPos, 0, 1, 2, 3, 9, 10, 12, 20.

data/rauch/function/game/ability/wokkaman/q/add_rocket.mcfunction
@@ -2,39 +2,56 @@ execute at @s run summon minecraft:item_display ~ ~ ~ {transformation:{left_rota
 execute as @s[team=Red] run tag @e[type=item_display,tag=wok_rocket,tag=setup] add red
 execute as @s[team=Blue] run tag @e[type=item_display,tag=wok_rocket,tag=setup] add blue
 
-scoreboard players operation t_mod2 global = @s wokkaman_rocket_count
-scoreboard players operation t_mod2 global %= 2 global
-scoreboard players operation t_mod4 global = @s wokkaman_rocket_count
-scoreboard players operation t_mod4 global %= 4 global
-scoreboard players operation t_mod8 global = @s wokkaman_rocket_count
-scoreboard players operation t_mod8 global %= 8 global
-scoreboard players operation t_mod16 global = @s wokkaman_rocket_count
-scoreboard players operation t_mod16 global %= 16 global
+scoreboard players set 2 const 2
+scoreboard players set 4 const 4
+scoreboard players set 8 const 8
+scoreboard players set 16 const 16
+scoreboard players set 32 const 32
+scoreboard players operation t_mod2 temp = @s wokkaman_rocket_count
+scoreboard players operation t_mod2 temp %= 2 const
+scoreboard players operation t_mod8 temp = @s wokkaman_rocket_count
+scoreboard players operation t_mod8 temp %= 4 const
+scoreboard players operation t_mod8 temp = @s wokkaman_rocket_count
+scoreboard players operation t_mod8 temp %= 8 const
+scoreboard players operation t_mod16 temp = @s wokkaman_rocket_count
+scoreboard players operation t_mod16 temp %= 16 const
+scoreboard players operation t_mod32 temp = @s wokkaman_rocket_count
+scoreboard players operation t_mod32 temp %= 32 const
 
-execute if score t_mod2 global matches 0 run tag @e[type=item_display,tag=wok_rocket,tag=setup] add clockwise
-execute if score t_mod2 global matches 1 run tag @e[type=item_display,tag=wok_rocket,tag=setup] add clockwise
-#execute if score t_mod2 global matches 1 run tag @e[type=item_display,tag=wok_rocket,tag=setup] add counter_clockwise
+execute if score t_mod2 temp matches 0 run tag @e[type=item_display,tag=wok_rocket,tag=setup] add clockwise
+execute if score t_mod2 temp matches 1 run tag @e[type=item_display,tag=wok_rocket,tag=setup] add clockwise
+#execute if score t_mod2 temp matches 1 run tag @e[type=item_display,tag=wok_rocket,tag=setup] add counter_clockwise
 
-scoreboard players operation t_wokkaman_rocket_rotating_tilt global = @s wokkaman_rocket_count
-scoreboard players operation t_wokkaman_rocket_rotating_tilt global += 4 global
-scoreboard players operation t_wokkaman_rocket_rotating_tilt global /= 4 global
-scoreboard players operation t_wokkaman_rocket_rotating_tilt global *= 35 global
-execute if score t_mod4 global matches 1..2 run scoreboard players operation t_wokkaman_rocket_rotating_tilt global *= -1 global
-scoreboard players operation @e[type=item_display,tag=wok_rocket,tag=setup] wokkaman_rocket_rotating_tilt = t_wokkaman_rocket_rotating_tilt global
+scoreboard players set -1 const -1
+scoreboard players set 12 const 12
+scoreboard players set 23 const 23
+scoreboard players set 35 const 35
+scoreboard players set 45 const 45
+scoreboard players set 90 const 90
+scoreboard players set 180 const 180
+scoreboard players operation t_wokkaman_rocket_rotating_tilt temp = t_mod8 temp
+scoreboard players operation t_wokkaman_rocket_rotating_tilt temp += 4 const
+scoreboard players operation t_wokkaman_rocket_rotating_tilt temp /= 4 const
+scoreboard players operation t_wokkaman_rocket_rotating_tilt temp *= 35 const
+execute if score t_mod8 temp matches 1..2 run scoreboard players operation t_wokkaman_rocket_rotating_tilt temp *= -1 const
+scoreboard players operation @e[type=item_display,tag=wok_rocket,tag=setup] wokkaman_rocket_rotating_tilt = t_wokkaman_rocket_rotating_tilt temp
 
-execute if score t_mod2 global matches 1 run scoreboard players operation t_wokkaman_rocket_phase global += 90 global
-execute if score t_mod4 global matches 2..3 run scoreboard players operation t_wokkaman_rocket_phase global += 180 global
-execute if score t_mod8 global matches 4..7 run scoreboard players operation t_wokkaman_rocket_phase global += 45 global
-execute if score t_mod16 global matches 8..15 run scoreboard players operation t_wokkaman_rocket_phase global += 23 global
-scoreboard players operation @e[type=item_display,tag=wok_rocket,tag=setup] wokkaman_rocket_phase = t_wokkaman_rocket_phase global
+execute if score t_mod2 temp matches 1 run scoreboard players operation t_wokkaman_rocket_phase temp += 90 const
+execute if score t_mod8 temp matches 2..3 run scoreboard players operation t_wokkaman_rocket_phase temp += 180 const
+execute if score t_mod8 temp matches 4..7 run scoreboard players operation t_wokkaman_rocket_phase temp += 45 const
+execute if score t_mod16 const matches 8..15 run scoreboard players operation t_wokkaman_rocket_phase temp += 23 const
+execute if score t_mod16 const matches 16..31 run scoreboard players operation t_wokkaman_rocket_phase temp += 12 const
+scoreboard players operation @e[type=item_display,tag=wok_rocket,tag=setup] wokkaman_rocket_phase = t_wokkaman_rocket_phase temp
 
 scoreboard players operation @e[type=item_display,tag=wok_rocket,tag=setup,limit=1] pnum = @s pnum
 tag @e[type=item_display,tag=wok_rocket,tag=setup] remove setup
 
-scoreboard players reset t_mod2 global
-scoreboard players reset t_mod4 global
-scoreboard players reset t_mod8 global
-scoreboard players reset t_wokkaman_rocket_phase global
-scoreboard players reset t_wokkaman_rocket_rotating_tilt global
+scoreboard players reset t_mod2 temp
+scoreboard players reset t_mod4 temp
+scoreboard players reset t_mod8 temp
+scoreboard players reset t_mod16 temp
+scoreboard players reset t_mod32 temp
+scoreboard players reset t_wokkaman_rocket_phase temp
+scoreboard players reset t_wokkaman_rocket_rotating_tilt temp
 
 scoreboard players add @s wokkaman_rocket_count 1

data/rauch/function/game/ability/wokkaman/q/rocket_rotate_airborne.mcfunction
@@ -6,15 +6,15 @@
 #calculate stuff idk yet
 execute at @a[tag=temp,limit=1] run summon marker ~ ~ ~ {Tags:["fuck"]}
 $execute as @e[type=marker,tag=fuck] at @s rotated $(delta_angle) 0 rotated ~-90 ~ positioned ^ ^ ^1.6 run tp @s ~ ~ ~ ~ ~
-execute store result score t_player_x global run data get entity @a[tag=temp,limit=1] Pos[0] 1000
-execute store result score t_player_z global run data get entity @a[tag=temp,limit=1] Pos[2] 1000
-execute store result score t_delta_x global run data get entity @e[type=marker,tag=fuck,limit=1] Pos[0] 1000
-execute store result score t_delta_z global run data get entity @e[type=marker,tag=fuck,limit=1] Pos[2] 1000
+execute store result score t_player_x temp run data get entity @a[tag=temp,limit=1] Pos[0] 1000
+execute store result score t_player_z temp run data get entity @a[tag=temp,limit=1] Pos[2] 1000
+execute store result score t_delta_x temp run data get entity @e[type=marker,tag=fuck,limit=1] Pos[0] 1000
+execute store result score t_delta_z temp run data get entity @e[type=marker,tag=fuck,limit=1] Pos[2] 1000
 # calculate deltas
-scoreboard players operation t_delta_x global -= t_player_x global
-scoreboard players operation t_delta_z global -= t_player_z global
-execute store result storage temp delta_x float 0.001 run scoreboard players get t_delta_x global
-execute store result storage temp delta_z float 0.001 run scoreboard players get t_delta_z global
+scoreboard players operation t_delta_x temp -= t_player_x temp
+scoreboard players operation t_delta_z temp -= t_player_z temp
+execute store result storage temp delta_x float 0.001 run scoreboard players get t_delta_x temp
+execute store result storage temp delta_z float 0.001 run scoreboard players get t_delta_z temp
 
 function rauch:game/ability/wokkaman/q/rocket_rotate_airborne_macro with storage temp
 
@@ -22,8 +22,8 @@ function rauch:game/ability/wokkaman/q/rocket_rotate_airborne_macro with storage
 execute at @s run particle minecraft:dust_color_transition{from_color:[1.0,0.8,0.3],scale:0.4,to_color:[0.5,0.0,0.0]} ^ ^ ^-0.3 0 0 0 0.01 1 force
 execute at @s run particle minecraft:small_flame ^ ^ ^-0.3 0.1 0.1 0.1 0.01 1 force
 
-scoreboard players reset t_player_x global
-scoreboard players reset t_player_z global
-scoreboard players reset t_delta_x global
-scoreboard players reset t_delta_z global
+scoreboard players reset t_player_x temp
+scoreboard players reset t_player_z temp
+scoreboard players reset t_delta_x temp
+scoreboard players reset t_delta_z temp
 kill @e[type=marker,tag=fuck]

data/rauch/function/game/ability/wokkaman/q/rocket_rotate_grounded.mcfunction
@@ -4,23 +4,23 @@ $execute at @s run tp @s ~ ~ ~ $(angle) 0
 #calculate height
 execute at @a[tag=temp] run summon marker ~ ~ ~ {Tags:["fuck"]}
 $execute as @e[type=marker,tag=fuck] at @s rotated $(delta_angle) 0 rotated ~90 0 positioned ^ ^ ^$(roll_angle) run tp @s ~ ~ ~ ~ ~
-execute store result score t_marker_z global run data get entity @e[type=marker,tag=fuck,limit=1] Pos[2] 1000
-execute store result score t_player_z global run data get entity @a[tag=temp,limit=1] Pos[2] 1000
-execute store result score t_old_y global run data get entity @s Pos[1] 1000
+execute store result score t_marker_z temp run data get entity @e[type=marker,tag=fuck,limit=1] Pos[2] 1000
+execute store result score t_player_z temp run data get entity @a[tag=temp,limit=1] Pos[2] 1000
+execute store result score t_old_y temp run data get entity @s Pos[1] 1000
 # calculate delta z (t_marker_z holds delta z after) (this is basically sine or cosine of the angle, not sure, times $(roll_angle))
-scoreboard players operation t_marker_z global -= t_player_z global
+scoreboard players operation t_marker_z temp -= t_player_z temp
 # add delta z to old y (t_marker_z holds value after)
-scoreboard players operation t_marker_z global += t_old_y global
+scoreboard players operation t_marker_z temp += t_old_y temp
 # set new y value
-execute store result entity @s Pos[1] double 0.001 run scoreboard players get t_marker_z global
+execute store result entity @s Pos[1] double 0.001 run scoreboard players get t_marker_z temp
 
 execute at @s run tp @s ^ ^ ^1.5
 execute at @s rotated ~ -90 run tp @s ~ ~ ~ ~ ~
-#execute if score t_is_grounded global matches 1 at @s run particle dust{color:[0.4,0.4,0.4],scale:0.6} ^ ^ ^-0.3 0 0 0 0 1 force
-#execute unless score t_is_grounded global matches 1 at @s run particle small_flame ^ ^ ^-0.3 0 0 0 0 1 force
+#execute if score t_is_grounded temp matches 1 at @s run particle dust{color:[0.4,0.4,0.4],scale:0.6} ^ ^ ^-0.3 0 0 0 0 1 force
+#execute unless score t_is_grounded temp matches 1 at @s run particle small_flame ^ ^ ^-0.3 0 0 0 0 1 force
 execute at @s run particle minecraft:dust_color_transition{from_color:[1.0,0.8,0.3],scale:0.4,to_color:[0.5,0.0,0.0]} ^ ^ ^-0.3 0 0 0 0.01 1 force
 
 kill @e[type=marker,tag=fuck]
-scoreboard players reset t_marker_z global
-scoreboard players reset t_player_z global
-scoreboard players reset t_old_y global
+scoreboard players reset t_marker_z temp
+scoreboard players reset t_player_z temp
+scoreboard players reset t_old_y temp

data/rauch/function/game/ability/wokkaman/sq/stepped_on.mcfunction
@@ -8,14 +8,16 @@ execute unless entity @a[tag=t_damager] run return run function rauch:game/abili
 execute at @s as @e[type=armor_stand,tag=stun] if score @s pnum = @p pnum at @s run tp @s ~ ~-0.2 ~
 execute at @s as @e[type=boat,tag=wok_boat] if score @s pnum = @p pnum run tp @s ~ ~ ~
 
+scoreboard players set 3 const 3
 tag @s add t_damage_target
-execute store result score .t_mod temp run time query gametime
-scoreboard players operation .t_mod temp %= 3 global
+execute store result score t_mod temp run time query gametime
+scoreboard players operation t_mod temp %= 3 const
 scoreboard players set .damage temp 4
-execute if score .t_mod temp matches 0 as @a[tag=t_damager] run function rauch:game/mechanics/damage_target_by_self
-execute if score .t_mod temp matches 0 at @s run particle minecraft:firework ~ ~ ~ 0.2 0.2 0.2 0.3 20 force
-execute if score .t_mod temp matches 0 at @s run particle minecraft:cherry_leaves ~ ~1 ~ 0.3 0.3 0.3 1 20 force
-execute if score .t_mod temp matches 0 at @s run particle minecraft:flash ~ ~ ~ 0 0 0 1 1 force
+execute if score t_mod temp matches 0 as @a[tag=t_damager] run function rauch:game/mechanics/damage_target_by_self
+execute if score t_mod temp matches 0 at @s run particle minecraft:firework ~ ~ ~ 0.2 0.2 0.2 0.3 20 force
+execute if score t_mod temp matches 0 at @s run particle minecraft:cherry_leaves ~ ~1 ~ 0.3 0.3 0.3 1 20 force
+execute if score t_mod temp matches 0 at @s run particle minecraft:flash ~ ~ ~ 0 0 0 1 1 force
+
+scoreboard players reset t_mod temp
 tag @s remove t_damage_target
 tag @a remove t_damager
-scoreboard players reset .t_mod temp

data/rauch/function/game/ability/wokkaman/wokkaman.mcfunction
@@ -18,9 +18,9 @@ execute as @s[tag=flying,tag=t_grounded] run function rauch:game/ability/wokkama
 execute as @s[scores={elytra=0..}] run function rauch:game/ability/wokkaman/f/elytra_cooldown
 
 scoreboard players add @s wokkaman_rocket_phase 1
-scoreboard players operation t_angle global = @s wokkaman_rocket_phase
-execute store result score t_player_angle global run data get entity @s Rotation[0]
-execute as @s[tag=t_grounded] run scoreboard players set t_is_grounded global 1
+scoreboard players operation t_angle temp = @s wokkaman_rocket_phase
+execute store result score t_player_angle temp run data get entity @s Rotation[0]
+execute as @s[tag=t_grounded] run scoreboard players set t_is_grounded temp 1
 
 scoreboard players operation t_pnum global = @s pnum
 tag @s add temp
@@ -28,8 +28,8 @@ execute as @e[type=item_display,tag=wok_rocket,tag=rotating] if score @s pnum = 
 tag @s remove temp
 scoreboard players reset t_pnum global
 
-scoreboard players reset t_angle global
-scoreboard players reset t_is_grounded global
-scoreboard players reset t_player_angle global
+scoreboard players reset t_angle temp
+scoreboard players reset t_is_grounded temp
+scoreboard players reset t_player_angle temp
 
 tag @s remove t_grounded

data/rauch/function/game/ability/flyer/q/hit.mcfunction
@@ -4,30 +4,23 @@ scoreboard players add @s stun 60
 function rauch:game/mechanics/stuninit
 
 # damage calculation (scale with buffs)
-scoreboard objectives add calc_damage dummy
-scoreboard objectives add 11 dummy
-scoreboard objectives add 48 dummy
-scoreboard players set @s 11 11
-scoreboard players set @s 48 48
+scoreboard players set 11 const 11
+scoreboard players set 48 const 48
 
 # read current damage from attributes (includes buffs)
-execute store result score @s calc_damage run attribute @p minecraft:generic.attack_damage get 1000
+execute store result score t_calc_damage temp run attribute @p minecraft:generic.attack_damage get 1000
 # 48 is the damage of the ground slam
-scoreboard players operation @s calc_damage *= @s 48
+scoreboard players operation t_calc_damage temp *= 48 const
 # 11 is normal damage of a pikka
-scoreboard players operation @s calc_damage /= @s 11
+scoreboard players operation t_calc_damage temp /= 11 const
 
-execute store result storage temp damage int 0.001 run scoreboard players get @s calc_damage
+execute store result storage temp damage int 0.001 run scoreboard players get t_calc_damage temp
 
 function rauch:game/ability/flyer/q/damage_macro with storage temp
 
 # add to damage dealt stats because /damage doesn't do that automatically
-scoreboard objectives add 500 dummy
-scoreboard players set @s 500 500
-scoreboard players operation @s calc_damage /= @s 500
-scoreboard players operation @s stats_g_damage_d += @s calc_damage
-scoreboard objectives remove 500
+scoreboard players set 500 const 500
+scoreboard players operation t_calc_damage temp /= 500 const
+scoreboard players operation @s stats_g_damage_d += t_calc_damage temp
 
-scoreboard objectives remove calc_damage
-scoreboard objectives remove 11
-scoreboard objectives remove 48
+scoreboard players reset t_calc_damage temp

commit 9f1485d2511f46442ea192b4b2480a9046027799

Wokkaman rocket changes, fixes for bugs introduced in f0a6ce2
Wokkaman Rockets:
- faster (0.96 m/s -> 1.26 m/s)
- changed particle effects
- made an performance optimization (varies, but on average about 25% less mspt)
- fixed rocket damage not working
- made it so when a Wokkaman sneaks in the air, the rockets rotate closer, making them more accurate cuz why not. Might not stay.

Fixed damage not working in a few other instances (because of f0a6ce2)

data/rauch/function/game/ability/wokkaman/q/add_rocket.mcfunction
@@ -1,6 +1,6 @@
 execute at @s run summon minecraft:item_display ~ ~ ~ {transformation:{left_rotation:[0f,0f,0f,1f],right_rotation:[0.707f,0f,0f,1f],translation:[0f,0f,0f],scale:[0.6f,0.6f,0.6f]},billboard:"fixed",Tags:["wok_rocket","rotating","setup"],item:{id:"minecraft:firework_rocket",count:1,components:{"minecraft:fireworks":{}}},teleport_duration:2}
 execute as @s[team=Red] run tag @e[type=item_display,tag=wok_rocket,tag=setup] add red
-execute as @s[team=Blue] run tag @e[type=item_display,tag=wok_rocket,tag=setup] add blue
+execute as @s[team=Blue] run tag @e[type=item_display,tag=wok_rocket,tag=setup] add blu
 
 scoreboard players set 2 const 2
 scoreboard players set 4 const 4

data/rauch/function/game/ability/wokkaman/q/explode.mcfunction
@@ -1,10 +1,14 @@
 #particle minecraft:dust_color_transition 0.7 0 0.3 1 0 0 0 ~ ~ ~ 2 2 2 0.1 700 force
-execute at @s run particle minecraft:firework ~ ~ ~ 0.1 0.1 0.1 0.7 100 force
-execute at @s run particle minecraft:firework ~ ~ ~ 0.1 0.1 0.1 0.25 200 force
-execute at @s run particle minecraft:electric_spark ~ ~ ~ 0.1 0.1 0.1 4 300 force
-execute at @s run particle minecraft:explosion ~ ~ ~ 1 1 1 1 20 force
-execute at @s run particle minecraft:flash ~ ~ ~ 0.1 0.1 0.1 1 1 force
-execute at @s run playsound minecraft:entity.firework_rocket.blast master @a ~ ~ ~ 1 1
+#execute at @s run particle minecraft:firework ~ ~ ~ 0.1 0.1 0.1 0.7 100 force
+#execute at @s run particle minecraft:firework ~ ~ ~ 0.1 0.1 0.1 0.25 200 force
+execute at @s run particle smoke ~ ~ ~ 0.1 0.1 0.1 0.4 50 force
+execute at @s run particle large_smoke ~ ~ ~ 0.1 0.1 0.1 0.25 100 force
+execute at @s run particle dust{color:[0.1,0.1,0.1],scale:1} ~ ~ ~ 2 2 2 1 200 force
+execute at @s run particle electric_spark ~ ~ ~ 0.1 0.1 0.1 4 300 force
+execute at @s run particle explosion ~ ~ ~ 1.4 1.4 1.4 1 10 force
+execute at @s run particle flash ~ ~ ~ 0.1 0.1 0.1 1 1 force
+execute at @s run playsound entity.firework_rocket.blast master @a ~ ~ ~ 1 1
+execute at @s run playsound minecraft:entity.firework_rocket.large_blast master @a ~ ~ ~ 1 1
 
 
 scoreboard players operation t_pnum global = @s pnum

data/rauch/function/game/ability/wokkaman/q/hit.mcfunction
@@ -1,6 +1,6 @@
 # t_pnum global has pnum of wokkaman, as player hit
 tag @s add t_damage_target
-scoreboard players set .damage temp 30
+scoreboard players set t_damage temp 30
 execute as @a if score @s pnum = t_pnum global run function rauch:game/mechanics/damage_target_by_self
 tag @s remove t_damage_target
 tag @a remove t_damager

data/rauch/function/game/ability/wokkaman/q/projectile.mcfunction
@@ -1,15 +1,30 @@
 scoreboard players add @s click 1
 
-execute at @s run tp @s ^ ^ ^0.32
-execute at @s run particle minecraft:firework ~ ~ ~ 0.05 0.05 0.05 0.02 2 force
+execute at @s run tp @s ^ ^ ^0.42
+#execute at @s run particle minecraft:firework ~ ~ ~ 0.05 0.05 0.05 0.02 2 force
+execute at @s run particle minecraft:smoke ~ ~ ~ 0.05 0.05 0.05 0.05 4 force
+#execute at @s run particle minecraft:flame ~ ~ ~ 0.05 0.05 0.05 0.02 1 force
+execute at @s run particle dust_color_transition{from_color:[1.0,0.4,0.4],scale:1,to_color:[0.2,0.0,0.0]} ~ ~ ~ 0.05 0.05 0.05 1 1 force
+execute at @s run particle dust_color_transition{from_color:[1.0,0.8,0.4],scale:1,to_color:[0.2,0.0,0.0]} ~ ~ ~ 0.05 0.05 0.05 1 1 force
+execute at @s run particle dust{color:[1.0,0.8,0.2],scale:1} ~ ~ ~ 0.05 0.05 0.05 1 1 force
 execute as @s[tag=!hit] run function rauch:game/ability/wokkaman/q/detect
 
-execute at @s run tp @s ^ ^ ^0.32
-execute at @s run particle minecraft:firework ~ ~ ~ 0.05 0.05 0.05 0.02 2 force
+execute at @s run tp @s ^ ^ ^0.42
+#execute at @s run particle minecraft:firework ~ ~ ~ 0.05 0.05 0.05 0.02 2 force
+execute at @s run particle minecraft:smoke ~ ~ ~ 0.05 0.05 0.05 0.05 4 force
+#execute at @s run particle minecraft:flame ~ ~ ~ 0.05 0.05 0.05 0.02 1 force
+execute at @s run particle dust_color_transition{from_color:[1.0,0.4,0.4],scale:1,to_color:[0.2,0.0,0.0]} ~ ~ ~ 0.05 0.05 0.05 1 1 force
+execute at @s run particle dust_color_transition{from_color:[1.0,0.8,0.4],scale:1,to_color:[0.2,0.0,0.0]} ~ ~ ~ 0.05 0.05 0.05 1 1 force
+execute at @s run particle dust{color:[1.0,0.8,0.2],scale:1} ~ ~ ~ 0.05 0.05 0.05 1 1 force
 execute as @s[tag=!hit] run function rauch:game/ability/wokkaman/q/detect
 
-execute at @s run tp @s ^ ^ ^0.32
-execute at @s run particle minecraft:firework ~ ~ ~ 0.05 0.05 0.05 0.02 2 force
+execute at @s run tp @s ^ ^ ^0.42
+#execute at @s run particle minecraft:firework ~ ~ ~ 0.05 0.05 0.05 0.02 2 force
+execute at @s run particle minecraft:smoke ~ ~ ~ 0.05 0.05 0.05 0.05 4 force
+#execute at @s run particle minecraft:flame ~ ~ ~ 0.05 0.05 0.05 0.02 1 force
+execute at @s run particle dust_color_transition{from_color:[1.0,0.4,0.4],scale:1,to_color:[0.2,0.0,0.0]} ~ ~ ~ 0.05 0.05 0.05 1 1 force
+execute at @s run particle dust_color_transition{from_color:[1.0,0.8,0.4],scale:1,to_color:[0.2,0.0,0.0]} ~ ~ ~ 0.05 0.05 0.05 1 1 force
+execute at @s run particle dust{color:[1.0,0.8,0.2],scale:1} ~ ~ ~ 0.05 0.05 0.05 1 1 force
 execute as @s[tag=!hit] run function rauch:game/ability/wokkaman/q/detect
 
 

data/rauch/function/game/ability/wokkaman/q/rocket_rotate_airborne.mcfunction
@@ -4,15 +4,18 @@
 
 
 #calculate stuff idk yet
+scoreboard players set 2 const 2
 execute at @a[tag=temp,limit=1] run summon marker ~ ~ ~ {Tags:["fuck"]}
 $execute as @e[type=marker,tag=fuck] at @s rotated $(delta_angle) 0 rotated ~-90 ~ positioned ^ ^ ^1.6 run tp @s ~ ~ ~ ~ ~
-execute store result score t_player_x temp run data get entity @a[tag=temp,limit=1] Pos[0] 1000
-execute store result score t_player_z temp run data get entity @a[tag=temp,limit=1] Pos[2] 1000
 execute store result score t_delta_x temp run data get entity @e[type=marker,tag=fuck,limit=1] Pos[0] 1000
 execute store result score t_delta_z temp run data get entity @e[type=marker,tag=fuck,limit=1] Pos[2] 1000
 # calculate deltas
 scoreboard players operation t_delta_x temp -= t_player_x temp
 scoreboard players operation t_delta_z temp -= t_player_z temp
+# sneak distance reduction:
+execute if score t_is_sneaking temp matches 1 run scoreboard players operation t_delta_x temp /= 2 const
+execute if score t_is_sneaking temp matches 1 run scoreboard players operation t_delta_z temp /= 2 const
+
 execute store result storage temp delta_x float 0.001 run scoreboard players get t_delta_x temp
 execute store result storage temp delta_z float 0.001 run scoreboard players get t_delta_z temp
 
@@ -22,8 +25,6 @@ function rauch:game/ability/wokkaman/q/rocket_rotate_airborne_macro with storage
 execute at @s run particle minecraft:dust_color_transition{from_color:[1.0,0.8,0.3],scale:0.4,to_color:[0.5,0.0,0.0]} ^ ^ ^-0.3 0 0 0 0.01 1 force
 execute at @s run particle minecraft:small_flame ^ ^ ^-0.3 0.1 0.1 0.1 0.01 1 force
 
-scoreboard players reset t_player_x temp
-scoreboard players reset t_player_z temp
 scoreboard players reset t_delta_x temp
 scoreboard players reset t_delta_z temp
 kill @e[type=marker,tag=fuck]

data/rauch/function/game/ability/wokkaman/q/rocket_rotate_airborne_macro.mcfunction
@@ -1,2 +1,3 @@
 $execute at @a[tag=temp,limit=1] positioned ~ ~1.6 ~ rotated ~ ~ run tp @s ^$(delta_z) ^$(delta_x) ^ ~ ~
-execute at @s run tp @s ^ ^ ^0.5
+execute unless score t_is_sneaking temp matches 1 at @s run tp @s ^ ^ ^0.5
+execute if score t_is_sneaking temp matches 1 at @s run tp @s ^ ^ ^0.6

data/rauch/function/game/ability/wokkaman/q/rocket_rotate_grounded.mcfunction
@@ -5,7 +5,6 @@ $execute at @s run tp @s ~ ~ ~ $(angle) 0
 execute at @a[tag=temp] run summon marker ~ ~ ~ {Tags:["fuck"]}
 $execute as @e[type=marker,tag=fuck] at @s rotated $(delta_angle) 0 rotated ~90 0 positioned ^ ^ ^$(roll_angle) run tp @s ~ ~ ~ ~ ~
 execute store result score t_marker_z temp run data get entity @e[type=marker,tag=fuck,limit=1] Pos[2] 1000
-execute store result score t_player_z temp run data get entity @a[tag=temp,limit=1] Pos[2] 1000
 execute store result score t_old_y temp run data get entity @s Pos[1] 1000
 # calculate delta z (t_marker_z holds delta z after) (this is basically sine or cosine of the angle, not sure, times $(roll_angle))
 scoreboard players operation t_marker_z temp -= t_player_z temp
@@ -22,5 +21,4 @@ execute at @s run particle minecraft:dust_color_transition{from_color:[1.0,0.8,0
 
 kill @e[type=marker,tag=fuck]
 scoreboard players reset t_marker_z temp
-scoreboard players reset t_player_z temp
 scoreboard players reset t_old_y temp

data/rauch/function/game/ability/wokkaman/q/rotate_rockets.mcfunction
@@ -1,0 +1,20 @@
+scoreboard players add @s wokkaman_rocket_phase 1
+scoreboard players operation t_angle temp = @s wokkaman_rocket_phase
+execute store result score t_player_angle temp run data get entity @s Rotation[0]
+execute as @s[tag=t_grounded] run scoreboard players set t_is_grounded temp 1
+execute as @s[predicate=rauch:sneak] run scoreboard players set t_is_sneaking temp 1
+
+scoreboard players operation t_pnum global = @s pnum
+execute store result score t_player_z temp run data get entity @s Pos[2] 1000
+execute as @s[tag=!t_grounded] store result score t_player_x temp run data get entity @s Pos[0] 1000
+tag @s add temp
+execute as @e[type=item_display,tag=wok_rocket,tag=rotating] if score @s pnum = t_pnum global at @s run function rauch:game/ability/wokkaman/q/rocket_rotate
+tag @s remove temp
+scoreboard players reset t_pnum global
+
+scoreboard players reset t_angle temp
+scoreboard players reset t_is_grounded temp
+scoreboard players reset t_is_sneaking temp
+scoreboard players reset t_player_angle temp
+scoreboard players reset t_player_z temp
+scoreboard players reset t_player_x temp

data/rauch/function/game/ability/wokkaman/sq/floor_reached.mcfunction
@@ -10,7 +10,7 @@ execute at @s run particle minecraft:campfire_cosy_smoke ~ ~1 ~ 2 1 2 0.03 300 f
 execute at @s run particle minecraft:explosion ~ ~1 ~ 2 1 2 0.1 10 force
 
 tag @s add t_damage_target
-scoreboard players set .damage temp 30
+scoreboard players set t_damage temp 30
 execute as @a[tag=t_damager] run function rauch:game/mechanics/damage_target_by_self
 tag @s remove t_damage_target
 tag @a remove t_damager

data/rauch/function/game/ability/wokkaman/sq/stepped_on.mcfunction
@@ -12,7 +12,7 @@ scoreboard players set 3 const 3
 tag @s add t_damage_target
 execute store result score t_mod temp run time query gametime
 scoreboard players operation t_mod temp %= 3 const
-scoreboard players set .damage temp 4
+scoreboard players set t_damage temp 4
 execute if score t_mod temp matches 0 as @a[tag=t_damager] run function rauch:game/mechanics/damage_target_by_self
 execute if score t_mod temp matches 0 at @s run particle minecraft:firework ~ ~ ~ 0.2 0.2 0.2 0.3 20 force
 execute if score t_mod temp matches 0 at @s run particle minecraft:cherry_leaves ~ ~1 ~ 0.3 0.3 0.3 1 20 force

data/rauch/function/game/ability/wokkaman/wokkaman.mcfunction
@@ -17,19 +17,6 @@ execute as @s[scores={fly=1..},tag=!flying] run tag @s add flying
 execute as @s[tag=flying,tag=t_grounded] run function rauch:game/ability/wokkaman/f/land
 execute as @s[scores={elytra=0..}] run function rauch:game/ability/wokkaman/f/elytra_cooldown
 
-scoreboard players add @s wokkaman_rocket_phase 1
-scoreboard players operation t_angle temp = @s wokkaman_rocket_phase
-execute store result score t_player_angle temp run data get entity @s Rotation[0]
-execute as @s[tag=t_grounded] run scoreboard players set t_is_grounded temp 1
-
-scoreboard players operation t_pnum global = @s pnum
-tag @s add temp
-execute as @e[type=item_display,tag=wok_rocket,tag=rotating] if score @s pnum = t_pnum global at @s run function rauch:game/ability/wokkaman/q/rocket_rotate
-tag @s remove temp
-scoreboard players reset t_pnum global
-
-scoreboard players reset t_angle temp
-scoreboard players reset t_is_grounded temp
-scoreboard players reset t_player_angle temp
+execute if score @s wokkaman_rocket_count matches 1.. run function rauch:game/ability/wokkaman/q/rotate_rockets
 
 tag @s remove t_grounded