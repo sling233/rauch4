sling233/rauch4

commit f0a6ce25b424453da608eacd86e620aeee09617b

Standardized const and temp scores (v606)
Numbers are always <number> const. Every time a number is used in an equasion it's score gets defined somewhere at the beginning of the function.
Temporary scores are always t_<something> temp. Temporary scores should always reset near the end of the function. I might change that to being cleared at the end of each tick.

THERE MIGHT BE REALLY WEIRD BUGS! I probably missed some things.

v606: removed objectives blu_calc, red_calc, xPos, yPos, zPos, 0, 1, 2, 3, 9, 10, 12, 20.

data/rauch/function/game/ability/raucher/r.mcfunction
@@ -1,23 +1,26 @@
 execute as @s[team=Red] at @s anchored eyes run summon armor_stand ^ ^ ^0.1 {Invulnerable:1b,Marker:0b,Invisible:1b,Tags:["red","r_s","setup","projectile"]}
 execute as @s[team=Blue] at @s anchored eyes run summon armor_stand ^ ^ ^0.1 {Invulnerable:1b,Marker:0b,Invisible:1b,Tags:["blu","r_s","setup","projectile"]}
 
-execute as @s at @s anchored eyes run summon armor_stand ^ ^ ^ {NoGravity:1b,Invulnerable:1b,Marker:1b,Invisible:1b,Tags:["vector"]}
+execute at @s anchored eyes run summon armor_stand ^ ^ ^ {NoGravity:1b,Invulnerable:1b,Marker:1b,Invisible:1b,Tags:["vector"]}
 
-execute as @s as @e[type=armor_stand,tag=r_s,tag=setup,limit=1,sort=nearest] store result score @s xPos run data get entity @s Pos[0] 1000000
-execute as @s as @e[type=armor_stand,tag=r_s,tag=setup,limit=1,sort=nearest] store result score @s yPos run data get entity @s Pos[1] 1000000
-execute as @s as @e[type=armor_stand,tag=r_s,tag=setup,limit=1,sort=nearest] store result score @s zPos run data get entity @s Pos[2] 1000000
+execute as @e[type=armor_stand,tag=r_s,tag=setup,limit=1,sort=nearest] store result score t_pos_x1 temp run data get entity @s Pos[0] 1000000
+execute as @e[type=armor_stand,tag=r_s,tag=setup,limit=1,sort=nearest] store result score t_pos_y1 temp run data get entity @s Pos[1] 1000000
+execute as @e[type=armor_stand,tag=r_s,tag=setup,limit=1,sort=nearest] store result score t_pos_z1 temp run data get entity @s Pos[2] 1000000
 
-execute as @s as @e[type=armor_stand,tag=vector,limit=1,sort=nearest] store result score @s xPos run data get entity @s Pos[0] 1000000
-execute as @s as @e[type=armor_stand,tag=vector,limit=1,sort=nearest] store result score @s yPos run data get entity @s Pos[1] 1000000
-execute as @s as @e[type=armor_stand,tag=vector,limit=1,sort=nearest] store result score @s zPos run data get entity @s Pos[2] 1000000
+execute as @e[type=armor_stand,tag=vector,limit=1,sort=nearest] store result score t_pos_x2 temp run data get entity @s Pos[0] 1000000
+execute as @e[type=armor_stand,tag=vector,limit=1,sort=nearest] store result score t_pos_y2 temp run data get entity @s Pos[1] 1000000
+execute as @e[type=armor_stand,tag=vector,limit=1,sort=nearest] store result score t_pos_z2 temp run data get entity @s Pos[2] 1000000
 
-execute as @s run scoreboard players operation @e[type=armor_stand,limit=1,tag=r_s,sort=nearest,tag=setup] xPos -= @e[type=armor_stand,limit=1,tag=vector,sort=nearest] xPos
-execute as @s run scoreboard players operation @e[type=armor_stand,limit=1,tag=r_s,sort=nearest,tag=setup] yPos -= @e[type=armor_stand,limit=1,tag=vector,sort=nearest] yPos
-execute as @s run scoreboard players operation @e[type=armor_stand,limit=1,tag=r_s,sort=nearest,tag=setup] zPos -= @e[type=armor_stand,limit=1,tag=vector,sort=nearest] zPos
+execute store result entity @e[type=armor_stand,tag=r_s,tag=setup,sort=nearest,limit=1] Motion[0] double 0.000017 run scoreboard players operation t_pos_x1 temp -= t_pos_x2 temp
+execute store result entity @e[type=armor_stand,tag=r_s,tag=setup,sort=nearest,limit=1] Motion[1] double 0.000017 run scoreboard players operation t_pos_y1 temp -= t_pos_y2 temp
+execute store result entity @e[type=armor_stand,tag=r_s,tag=setup,sort=nearest,limit=1] Motion[2] double 0.000017 run scoreboard players operation t_pos_z1 temp -= t_pos_z2 temp
 
-execute as @s as @e[type=armor_stand,tag=r_s,tag=setup,sort=nearest] store result entity @s Motion[0] double 0.000017 run scoreboard players get @s xPos
-execute as @s as @e[type=armor_stand,tag=r_s,tag=setup,sort=nearest] store result entity @s Motion[1] double 0.000017 run scoreboard players get @s yPos
-execute as @s as @e[type=armor_stand,tag=r_s,tag=setup,sort=nearest] store result entity @s Motion[2] double 0.000017 run scoreboard players get @s zPos
+scoreboard players reset t_pos_x1 temp
+scoreboard players reset t_pos_y1 temp
+scoreboard players reset t_pos_z1 temp
+scoreboard players reset t_pos_x2 temp
+scoreboard players reset t_pos_y2 temp
+scoreboard players reset t_pos_z2 temp
 
 kill @e[type=armor_stand,tag=vector]
 tag @e[type=armor_stand,tag=r_s,tag=setup] remove setup

data/rauch/function/game/ability/raucher/sr.mcfunction
@@ -3,21 +3,24 @@ execute as @s[team=Blue] at @s anchored eyes run summon armor_stand ^ ^ ^0.1 {In
 
 execute as @s at @s anchored eyes run summon area_effect_cloud ^ ^ ^ {NoGravity:1b,Invulnerable:1b,Marker:1b,Invisible:1b,Tags:["vector"]}
 
-execute as @s as @e[type=armor_stand,tag=r_h,tag=setup,limit=1,sort=nearest] store result score @s xPos run data get entity @s Pos[0] 1000000
-execute as @s as @e[type=armor_stand,tag=r_h,tag=setup,limit=1,sort=nearest] store result score @s yPos run data get entity @s Pos[1] 1000000
-execute as @s as @e[type=armor_stand,tag=r_h,tag=setup,limit=1,sort=nearest] store result score @s zPos run data get entity @s Pos[2] 1000000
+execute as @e[type=armor_stand,tag=r_h,tag=setup,limit=1,sort=nearest] store result score t_pos_x1 temp run data get entity @s Pos[0] 1000000
+execute as @e[type=armor_stand,tag=r_h,tag=setup,limit=1,sort=nearest] store result score t_pos_y1 temp run data get entity @s Pos[1] 1000000
+execute as @e[type=armor_stand,tag=r_h,tag=setup,limit=1,sort=nearest] store result score t_pos_z1 temp run data get entity @s Pos[2] 1000000
 
-execute as @s as @e[type=area_effect_cloud,tag=vector,limit=1,sort=nearest] store result score @s xPos run data get entity @s Pos[0] 1000000
-execute as @s as @e[type=area_effect_cloud,tag=vector,limit=1,sort=nearest] store result score @s yPos run data get entity @s Pos[1] 1000000
-execute as @s as @e[type=area_effect_cloud,tag=vector,limit=1,sort=nearest] store result score @s zPos run data get entity @s Pos[2] 1000000
+execute as @e[type=armor_stand,tag=vector,limit=1,sort=nearest] store result score t_pos_x2 temp run data get entity @s Pos[0] 1000000
+execute as @e[type=armor_stand,tag=vector,limit=1,sort=nearest] store result score t_pos_y2 temp run data get entity @s Pos[1] 1000000
+execute as @e[type=armor_stand,tag=vector,limit=1,sort=nearest] store result score t_pos_z2 temp run data get entity @s Pos[2] 1000000
 
-execute as @s run scoreboard players operation @e[type=armor_stand,limit=1,tag=r_h,sort=nearest,tag=setup] xPos -= @e[type=area_effect_cloud,limit=1,tag=vector,sort=nearest] xPos
-execute as @s run scoreboard players operation @e[type=armor_stand,limit=1,tag=r_h,sort=nearest,tag=setup] yPos -= @e[type=area_effect_cloud,limit=1,tag=vector,sort=nearest] yPos
-execute as @s run scoreboard players operation @e[type=armor_stand,limit=1,tag=r_h,sort=nearest,tag=setup] zPos -= @e[type=area_effect_cloud,limit=1,tag=vector,sort=nearest] zPos
+execute store result entity @e[type=armor_stand,tag=r_h,tag=setup,sort=nearest,limit=1] Motion[0] double 0.000017 run scoreboard players operation t_pos_x1 temp -= t_pos_x2 temp
+execute store result entity @e[type=armor_stand,tag=r_h,tag=setup,sort=nearest,limit=1] Motion[1] double 0.000017 run scoreboard players operation t_pos_y1 temp -= t_pos_y2 temp
+execute store result entity @e[type=armor_stand,tag=r_h,tag=setup,sort=nearest,limit=1] Motion[2] double 0.000017 run scoreboard players operation t_pos_z1 temp -= t_pos_z2 temp
 
-execute as @s as @e[type=armor_stand,tag=r_h,tag=setup,sort=nearest] store result entity @s Motion[0] double 0.000017 run scoreboard players get @s xPos
-execute as @s as @e[type=armor_stand,tag=r_h,tag=setup,sort=nearest] store result entity @s Motion[1] double 0.000017 run scoreboard players get @s yPos
-execute as @s as @e[type=armor_stand,tag=r_h,tag=setup,sort=nearest] store result entity @s Motion[2] double 0.000017 run scoreboard players get @s zPos
+scoreboard players reset t_pos_x1 temp
+scoreboard players reset t_pos_y1 temp
+scoreboard players reset t_pos_z1 temp
+scoreboard players reset t_pos_x2 temp
+scoreboard players reset t_pos_y2 temp
+scoreboard players reset t_pos_z2 temp
 
 kill @e[type=area_effect_cloud,tag=vector]
 tag @e[type=armor_stand,tag=r_h,tag=setup] remove setup

data/rauch/function/game/ability/wark/r.mcfunction
@@ -1,10 +1,9 @@
 execute at @s[team=Red] run function rauch:game/ability/wark/r/detectred
 execute at @s[team=Blue] run function rauch:game/ability/wark/r/detectblue
 
-scoreboard objectives add t_min_health dummy
-scoreboard players set Global t_min_health 9999999
+scoreboard players set t_min_health temp 9999999
 execute as @a[tag=temp] run function rauch:game/ability/wark/r/evaluate
-scoreboard objectives remove t_min_health
+scoreboard players reset t_min_health temp
 execute at @s as @a[tag=least,limit=1] run function rauch:game/ability/wark/r/hit
 
 execute if entity @a[tag=least] at @s run playsound minecraft:entity.warden.listening_angry master @a

commit 86d71aebe408fa3e676e002ac0d140077980b504

Fixed Raucher sr not launching properly (broke in f0a6ce2)

data/rauch/function/game/ability/raucher/r.mcfunction
@@ -1,15 +1,15 @@
 execute as @s[team=Red] at @s anchored eyes run summon armor_stand ^ ^ ^0.1 {Invulnerable:1b,Marker:0b,Invisible:1b,Tags:["red","r_s","setup","projectile"]}
 execute as @s[team=Blue] at @s anchored eyes run summon armor_stand ^ ^ ^0.1 {Invulnerable:1b,Marker:0b,Invisible:1b,Tags:["blu","r_s","setup","projectile"]}
 
-execute at @s anchored eyes run summon armor_stand ^ ^ ^ {NoGravity:1b,Invulnerable:1b,Marker:1b,Invisible:1b,Tags:["vector"]}
+execute at @s anchored eyes run summon marker ^ ^ ^ {Tags:["vector"]}
 
 execute as @e[type=armor_stand,tag=r_s,tag=setup,limit=1,sort=nearest] store result score t_pos_x1 temp run data get entity @s Pos[0] 1000000
 execute as @e[type=armor_stand,tag=r_s,tag=setup,limit=1,sort=nearest] store result score t_pos_y1 temp run data get entity @s Pos[1] 1000000
 execute as @e[type=armor_stand,tag=r_s,tag=setup,limit=1,sort=nearest] store result score t_pos_z1 temp run data get entity @s Pos[2] 1000000
 
-execute as @e[type=armor_stand,tag=vector,limit=1,sort=nearest] store result score t_pos_x2 temp run data get entity @s Pos[0] 1000000
-execute as @e[type=armor_stand,tag=vector,limit=1,sort=nearest] store result score t_pos_y2 temp run data get entity @s Pos[1] 1000000
-execute as @e[type=armor_stand,tag=vector,limit=1,sort=nearest] store result score t_pos_z2 temp run data get entity @s Pos[2] 1000000
+execute as @e[type=marker,tag=vector,limit=1,sort=nearest] store result score t_pos_x2 temp run data get entity @s Pos[0] 1000000
+execute as @e[type=marker,tag=vector,limit=1,sort=nearest] store result score t_pos_y2 temp run data get entity @s Pos[1] 1000000
+execute as @e[type=marker,tag=vector,limit=1,sort=nearest] store result score t_pos_z2 temp run data get entity @s Pos[2] 1000000
 
 execute store result entity @e[type=armor_stand,tag=r_s,tag=setup,sort=nearest,limit=1] Motion[0] double 0.000017 run scoreboard players operation t_pos_x1 temp -= t_pos_x2 temp
 execute store result entity @e[type=armor_stand,tag=r_s,tag=setup,sort=nearest,limit=1] Motion[1] double 0.000017 run scoreboard players operation t_pos_y1 temp -= t_pos_y2 temp
@@ -22,7 +22,7 @@ scoreboard players reset t_pos_x2 temp
 scoreboard players reset t_pos_y2 temp
 scoreboard players reset t_pos_z2 temp
 
-kill @e[type=armor_stand,tag=vector]
+kill @e[type=marker,tag=vector]
 tag @e[type=armor_stand,tag=r_s,tag=setup] remove setup
 
 execute at @s run playsound minecraft:entity.player.attack.sweep master @a

data/rauch/function/game/ability/raucher/sr.mcfunction
@@ -1,15 +1,15 @@
 execute as @s[team=Red] at @s anchored eyes run summon armor_stand ^ ^ ^0.1 {Invulnerable:1b,Marker:0b,Invisible:1b,Tags:["red","r_h","setup","projectile"]}
 execute as @s[team=Blue] at @s anchored eyes run summon armor_stand ^ ^ ^0.1 {Invulnerable:1b,Marker:0b,Invisible:1b,Tags:["blu","r_h","setup","projectile"]}
 
-execute as @s at @s anchored eyes run summon area_effect_cloud ^ ^ ^ {NoGravity:1b,Invulnerable:1b,Marker:1b,Invisible:1b,Tags:["vector"]}
+execute at @s anchored eyes run summon marker ^ ^ ^ {Tags:["vector"]}
 
 execute as @e[type=armor_stand,tag=r_h,tag=setup,limit=1,sort=nearest] store result score t_pos_x1 temp run data get entity @s Pos[0] 1000000
 execute as @e[type=armor_stand,tag=r_h,tag=setup,limit=1,sort=nearest] store result score t_pos_y1 temp run data get entity @s Pos[1] 1000000
 execute as @e[type=armor_stand,tag=r_h,tag=setup,limit=1,sort=nearest] store result score t_pos_z1 temp run data get entity @s Pos[2] 1000000
 
-execute as @e[type=armor_stand,tag=vector,limit=1,sort=nearest] store result score t_pos_x2 temp run data get entity @s Pos[0] 1000000
-execute as @e[type=armor_stand,tag=vector,limit=1,sort=nearest] store result score t_pos_y2 temp run data get entity @s Pos[1] 1000000
-execute as @e[type=armor_stand,tag=vector,limit=1,sort=nearest] store result score t_pos_z2 temp run data get entity @s Pos[2] 1000000
+execute as @e[type=marker,tag=vector,limit=1,sort=nearest] store result score t_pos_x2 temp run data get entity @s Pos[0] 1000000
+execute as @e[type=marker,tag=vector,limit=1,sort=nearest] store result score t_pos_y2 temp run data get entity @s Pos[1] 1000000
+execute as @e[type=marker,tag=vector,limit=1,sort=nearest] store result score t_pos_z2 temp run data get entity @s Pos[2] 1000000
 
 execute store result entity @e[type=armor_stand,tag=r_h,tag=setup,sort=nearest,limit=1] Motion[0] double 0.000017 run scoreboard players operation t_pos_x1 temp -= t_pos_x2 temp
 execute store result entity @e[type=armor_stand,tag=r_h,tag=setup,sort=nearest,limit=1] Motion[1] double 0.000017 run scoreboard players operation t_pos_y1 temp -= t_pos_y2 temp
@@ -22,7 +22,7 @@ scoreboard players reset t_pos_x2 temp
 scoreboard players reset t_pos_y2 temp
 scoreboard players reset t_pos_z2 temp
 
-kill @e[type=area_effect_cloud,tag=vector]
+kill @e[type=marker,tag=vector]
 tag @e[type=armor_stand,tag=r_h,tag=setup] remove setup
 
 execute at @s run playsound minecraft:entity.player.attack.sweep master @a